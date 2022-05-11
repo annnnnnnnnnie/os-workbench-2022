b4034242463b259f1343d994aae7ed671d67aed0
tracer-nju <tracer@nju.edu.cn>
2022-05-11 20:57:42 +0800

    Linux DESKTOP-IC0PRDQ 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:57:42 up  1:27,  0 users,  load average: 0.00, 0.02, 0.00

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <assert.h>
 
+static void print_help_info();
+
 int main(int argc, char *argv[]) {
   for (int i = 0; i < argc; i++) {
+    // If -V or --version is set, print version info to stderr and exit
     assert(argv[i]);
     printf("argv[%d] = %s\n", i, argv[i]);
+    print_help_info();
   }
   assert(!argv[argc]);
   return 0;
+}
+
+static void print_help_info() {
+  char *HELP_TEXT = "pstree\n"
+                    "Copyleft (C) 0000-0000";
+  fprintf(stderr, HELP_TEXT);
 }
