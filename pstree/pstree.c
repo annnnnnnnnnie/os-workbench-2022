5e68f0832462eddf539cfcfc1de35ec3aa95c212
tracer-nju <tracer@nju.edu.cn>
2022-05-11 21:05:21 +0800

    Linux DESKTOP-IC0PRDQ 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:05:21 up  1:35,  0 users,  load average: 0.03, 0.03, 0.00

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -10,8 +10,8 @@ int main(int argc, char *argv[]) {
     assert(argv[i]);
     if (strncmp("-V", argv[i], 3) == 0 || strncmp("--version", argv[i], strlen("--version")) == 0){
       print_version_info();
-      printf("%zu\n", strlen("-V"));
     }
+    printf("%zu\n", strlen("-V"));
     printf("argv[%d] = %s\n", i, argv[i]);
   }
   assert(!argv[argc]);
