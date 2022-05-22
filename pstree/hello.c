9c77884eece720b1901135013a1cd26fba117fd2
ann <2330610648@qq.com>
2022-05-22 22:10:08 +0800

    rm hello.c

--- pstree/hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-    const char *name_str = "Name:";
-    printf("size of name_str is %ld\n", sizeof(*name_str));
-    const char buf[512];
-    printf("size of buf is %ld\n", sizeof(buf));
-}
